bc7de39acfd7bb5d7bbc77e00495b85afc68cce8
bartekglowacz <bartekglowacz@gmail.com>
2023-03-16 13:13:16 +0100

    gra w "wisielca"

---
+++ Hangman.py
@@ -0,0 +1,21 @@
+"""
+Gra w "wisielca". Jeden z użytkowników wprowadza słowo, które drugi z użytkowników ma odgadnąć.
+Program wyrysowuje tyle "_" ile liter ma podane słowo.
+Odgadujący podaje literka po literce. Jeżeli litera występuje - należy zamienić "_" na literę (lub litery, jeżeli jest ich więcej).
+Jeżeli litera nie występuje to należy zacząć rysowanie szubienicy i wisielca.
+Kolejność rysowania szubienicy:
+1. Noga podstawy
+2. Druga noga podstawy
+3. Podstawa
+4. Pionowy maszt wystający ze środka podstawy
+5. Pozioma belka, do której przywiązany będzie sznur
+6. Ukosna belka (wspornik) podtrzymująca belkę poziomą
+7. Sznur
+Kolejność rysowania wisielca:
+1. Głowa
+2. Tułów
+3. Ręka
+5. Druga ręka
+6. Noga
+7. Druga noga
+"""
