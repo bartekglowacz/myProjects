1e99fb06acedd40c04d52c60f2a84e97bc0557c4
bartekglowacz <bartekglowacz@gmail.com>
2023-04-07 21:45:42 +0200

    .

--- tmp.py
+++ tmp.py
@@ -1,26 +1,27 @@
-import random
+class RockPaperScissors:
+    # reszta kodu jest taka sama jak w Twoim przykładzie
 
-choice = random.randint(1, 3)
-if choice == 1:
-    rock = '''
-      _______________
-     /               \\
-    |       ROCK     |
-    \________________/
-    '''
-    print(rock)
-if choice == 2:
-    rock = '''
-        /^\\
-       /   \\
-      /     \\
-      |     |
-      |     |
-      |     |
-      |     |
-      |     |
-    /  \   /  \\
-   |    |  |   |
-   ------  -----
-    '''
-    print(rock)
+    def Play(self):
+        list_of_choices = [[self.list_of_players[player], self.SetSign()] for player in
+                           range(0, len(self.list_of_players))]
+        print(list_of_choices)
+
+        # utworzenie listy zawierającej tylko wybory graczy
+        player_choices = [choice[1] for choice in list_of_choices]
+
+        # decydowanie o zwycięzcy
+        if "rock" in player_choices and "paper" in player_choices and "scissors" in player_choices:
+            print("Remis")
+        elif "rock" in player_choices and "paper" in player_choices:
+            print("Papier owija kamień!")
+        elif "rock" in player_choices and "scissors" in player_choices:
+            print("Kamień tępi nożyce!")
+        elif "paper" in player_choices and "scissors" in player_choices:
+            print("Nożyce tną papier!")
+        else:
+            print("Nieznany wynik gry")
+
+        for x in range(0, len(list_of_choices)):
+            player_name = list_of_choices[x][0]
+            player_choice = list_of_choices[x][1]
+            print(f"Gracz {player_name} wybrał {player_choice}")
