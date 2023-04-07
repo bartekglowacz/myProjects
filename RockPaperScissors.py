76cda416e9ac7cf1296527b3805f034a9194c588
bartekglowacz <bartekglowacz@gmail.com>
2023-04-07 21:46:22 +0200

    ok

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -6,24 +6,38 @@ class RockPaperScissors:
         self.list_of_players = None
 
     def Players(self):
-        print("Ilu graczy bierze udział w grze?")
-        number_of_players = int(input())
+        number_of_players = 2
         self.list_of_players = ["Player" + str(i) for i in range(1, number_of_players + 1)]
         return self.list_of_players
 
     def SetSign(self):
         choice = random.randint(1, 3)
         if choice == 1:
-            sign = "rock"
+            sign = "kamień"
         if choice == 2:
-            sign = "paper"
+            sign = "papier"
         if choice == 3:
-            sign = "scissors"
+            sign = "nożyce"
         return sign
 
     def Play(self):
-        for player in range(0, len(self.list_of_players)):
-            print(f"Gracz {self.list_of_players[player]} wybrał {self.SetSign()}")
+        list_of_choices = [[self.list_of_players[player], self.SetSign()] for player in
+                           range(0, len(self.list_of_players))]
+        print(list_of_choices)
+        for x in range(0, len(list_of_choices)):
+            player_name = list_of_choices[x][0]
+            player_choice = list_of_choices[x][1]
+            # result_list.append(player_choice)
+            print(f"Gracz {player_name} wybrał {player_choice}")
+        result_list = [choice[1] for choice in list_of_choices]
+        if "kamień" in result_list and "papier" in result_list:
+            print("Papier owija kamień!")
+        elif "kamień" in result_list and "nożyce" in result_list:
+            print("Kamień tępi nożyce!")
+        elif "papier" in result_list and "nożyce" in result_list:
+            print("Nożyce tną papier!")
+        else:
+            print("Remis!")
 
 
 game = RockPaperScissors()
