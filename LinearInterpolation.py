bd01cd8c91dea94f1417b5fa58fb09355dddc6d4
bartekglowacz <bartekglowacz@gmail.com>
2023-11-17 13:10:05 +0100

    z zadanego pliku program wyciąga kolumnę z częstotliwością i poziomem

--- LinearInterpolation.py
+++ LinearInterpolation.py
@@ -9,8 +9,14 @@ def read_original_freq_and_level():
     original_file = open("original_freq_and_level.txt", "r")
     original_frequency = []
     original_level = []
-    original_frequency = original_file.readline().split("\t")
-    print(f"f = {original_frequency[0]}")
+    tmp = original_file.readlines()
+    # print(f"TMP: {tmp}")
+    for x in range(0, len(tmp)):
+        # print(tmp[x])
+        original_frequency.append(float(tmp[x].split("\t")[0]))
+        original_level.append(float(tmp[x].split("\t")[1].replace(",", ".")))
+    print(f"Oryginalne częstotliwości: {original_frequency}")
+    print(f"Oryginalne poziomy: {original_level}")
 
 
 read_original_freq_and_level()
